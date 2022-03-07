e1423ff9d835db65feb3faeecd7676a8b76b6fb8
bagus25dzikri06 <bagusdzikri@gmail.com>
2022-03-07 14:12:26 +0700

    Tugas 2 Nomor 1 (Object Biodata)

---
+++ javascript-1-dan-2/tugas2Nomor1.js
@@ -0,0 +1,63 @@
+const biodata = {
+    name: 'Bagus Dzikri Hidayat',
+    age: 18,
+    hobbies: ['reading', 'listening to the music', 'browsing'],
+    isMarried: false,
+    schoolList: [
+        {
+            name: 'SD Al-Irsyad 01 Cilacap',
+            yearIn: 2001,
+            yearOut: 2005,
+            major: null
+        },
+        {
+            name: 'SD Negeri 03 Pengasinan',
+            yearIn: 2005,
+            yearOut: 2007,
+            major: null
+        },
+        {
+            name: 'SMP Negeri 10 Depok',
+            yearIn: 2007,
+            yearOut: 2010,
+            major: null
+        },
+        {
+            name: 'SMA Yadika 12 Limo',
+            yearIn: 2010,
+            yearOut: 2013,
+            major: 'Natural Science'
+        },
+        {
+            name: 'Institut Pertanian Bogor',
+            yearIn: 2013,
+            yearOut: 2016,
+            major: 'Computer Science'
+        },
+        {
+            name: 'Universitas Nasional',
+            yearIn: 2016,
+            yearOut: 2019,
+            major: 'Information Systems'
+        }
+    ],
+    skills: [
+        {
+            skillName: 'JavaScript',
+            level: 'advanced'
+        },
+        {
+            skillName: 'Java',
+            level: 'advanced'
+        },
+        {
+            skillName: 'MySQL',
+            level: 'advanced'
+        },
+        {
+            skillName: 'MongoDB',
+            level: 'beginner'
+        }
+    ],
+    interestedInCoding: true
+}
