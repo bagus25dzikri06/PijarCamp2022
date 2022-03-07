bf8b9f709764272b5ee447d3be14d495a2099488
bagus25dzikri06 <bagusdzikri@gmail.com>
2022-03-07 14:21:56 +0700

    Tugas 2 Nomor 4 (Spread Operator dan Object Destructuring)

---
+++ javascript-1-dan-2/tugas2Nomor4.js
@@ -0,0 +1,31 @@
+let data = {
+    id: 1,
+	name: "Leanne Graham",
+	username: "Bret",
+	email: "Sincere@april.biz",
+	address: 
+	{
+		street: "Kulas Light",
+		suite: "Apt. 556",
+		city: "Gwenborough",
+		zipcode: "92998-3874",
+	},
+	phone: "1-770-736-8031 x56442",
+	website: "hildegard.org",   
+}
+
+//a. Ganti data nama, e-mail dan hobi menggunakan spread operator
+let dataBaru = {
+    ...data,
+    name: 'Bagus Dzikri Hidayat',
+    email: 'bagusdzikri@gmail.com',
+    hobby: 'Reading, listening to the music, browsing'
+}
+
+console.log(dataBaru)
+
+//b. Ambillah data “street dan city” tersebut menggunakan destructuring
+const { address } = dataBaru
+
+console.log(address.street)
+console.log(address.city)
