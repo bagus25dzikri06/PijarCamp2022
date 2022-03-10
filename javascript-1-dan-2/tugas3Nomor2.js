53812502c8b008eb11b151ae5a4198a7e7206c82
bagus25dzikri06 <bagusdzikri@gmail.com>
2022-03-10 15:03:27 +0700

    Tugas 3 Nomor 2 (Cari Nama Berdasarkan Data Nama Di Suatu Array)

---
+++ javascript-1-dan-2/tugas3Nomor2.js
@@ -0,0 +1,36 @@
+const searchName = (substringNama, jumlahNama, arrayNama, limitedArray, capitalizedArr) => {
+    let dataNama = ''
+    let arrayNamainLowercase = arrayNama.map((e) => {
+        return e.toLowerCase()
+    })
+    for (let i = 0; i < arrayNamainLowercase.length; i++) {
+        if ( arrayNamainLowercase[i].includes(substringNama) === true ) {
+            dataNama += arrayNamainLowercase[i] + ', '
+        } else {
+            continue
+        }
+    }
+
+    limitedArray(dataNama.split(', '), jumlahNama, capitalizedArr)
+}
+
+const limitedArray = (arr, num, capitalizedArr) => {
+    let limitArr = []
+    for (let i = 0; i < num; i++) {
+        limitArr.push(arr[i])
+    } 
+    capitalizedArr(limitArr)
+}
+
+const capitalizedArr = (arr) => {
+    console.log(arr.map((e) => {
+        return e.charAt(0).toUpperCase() + e.substring(1)
+    }))
+}
+
+searchName('an', 3, [
+    'Abigail', 'Alexandra', 'Alison', 'Amanda',
+    'Angela', 'Bella', 'Carol', 'Caroline',
+    'Carolyn', 'Deirdre', 'Diana', 'Elizabeth',
+    'Ella', 'Faith', 'Olivia', 'Penelope'
+], limitedArray, capitalizedArr)
